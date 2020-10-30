bddd911683f1667af8e6ca829dd7d93f7cbfb600
LewRit <lewrit16@gmail.com>
2020-10-29 20:03:13 -0400

    start passport

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express=require('express');
 const app=express();
-const PORT=3000
+const session=require('express-session')
+const passport=require('passport')
+
+app.use(session({
+    secret:'super-secret'||'dev',
+    cookie:{maxAge:60000}
+}))
 
 
 
@@ -9,7 +15,6 @@ const PORT=3000
 
 
 
-
-app.listen(PORT,()=>{
-    console.log(`the port is open at port ${PORT}`)
+app.listen(process.env.PORT,()=>{
+    console.log(`The server is running`)
 })
