dd4c5590e04c3167ad45df72f6a09c22f62b1aa2
LewRit <lewrit16@gmail.com>
2020-11-04 16:34:28 -0500

    add refresh code

--- auth/auth.js
+++ auth/auth.js
@@ -1,24 +1,44 @@
 const express=require('express')
 const auth=express.Router()
 const passport=require('passport')
+const refresh=require('passport-oauth2-refresh')
 const SpotifyStrategy = require('passport-spotify').Strategy;
 
-passport.use(
-  new SpotifyStrategy(
+
+const strategy=  new SpotifyStrategy(
     {
       clientID: process.env.SPOT_ID,
       clientSecret: process.env.SPOT_PASS,
       callbackURL: process.env.SPOT_CALL
     },
     function(accessToken, refreshToken, expires_in, profile, done) {
-      //  User.findOrCreate({ spotifyId: profile.id }, function(err, user) {
-      //   return done(err, user);
-      // });
+      User.findOrCreate({ 
+      where: {
+        spotifyId: profile.id,
+        username: profile.username,
+        country: profile.country
+       },
+      }).then(([user, created]) => {
+         // store access token
+        User.update({
+          spotifyAccessToken: accessToken
+        }, {
+          where: {
+            id: user.id
+          },
+          returning: true // returns the user after update
+        }).then(result => {
+          user = result[1][0]; // get user
+
+          done(null, user)
+        })
       console.log("Access Token: "+ accessToken)
       done(null,profile)
+    })
     }
-  )
 );
+passport.use(strategy);
+refresh.use(stratgey);
 
 auth.get('/spotify',passport.authenticate('spotify'));
 
