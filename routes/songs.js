ea77f01dc587009569bf15db95de5c6b434ab810
LewRit <lewrit16@gmail.com>
2020-11-07 11:42:01 -0500

    pull songs from DB

--- routes/songs.js
+++ routes/songs.js
@@ -1,7 +1,23 @@
 const express=require('express')
 const router=express.Router()
+const db=require('../models')
 
-
-router.get('/api/songs',(req,res)=>{
-    
+router.get('/songs',async (req,res)=>{
+    let songList=await db.song.findAll()
+    res.send(songList)
 })
+router.get('/songs/:id', async(req,res)=>{
+    let songID=parseInt(req.params.id)
+    const song=await db.song.findByPk(songID);
+
+    if (song){
+        res.send(song)
+    } else{
+        res.status(404).send('Song not found')
+    }
+})
+
+
+
+
+module.exports=router
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 app.use('/auth',auth)
+app.use('/api', router)
 
 app.use('/', express.static( __dirname + '/public'))
 app.use('/js', express.static(__dirname + '/js'))
